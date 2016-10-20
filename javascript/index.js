1e89bbabc72dd8a2b4a27377da01127708560672
Adrian Jarc <adrianjarc1@gmail.com>
2016-10-20 15:07:05 +0200

    Add 'authentication' and redirecton to next page

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,36 @@
+var users = [
+  {
+    'id': 1,
+    'username': 'adrianj',
+    'password': 'adrian1234'
+  },
+  {
+    'id': 2,
+    'username': 'janezn',
+    'password': 'novak4321'
+  }
+]
+
+
+
 var login = function () {
-  console.log("Heya", window.location);
+  var uname = document.forms['loginForm']['username'].value;
+  var pass = document.forms['loginForm']['password'].value;
+
+  var error = document.getElementsByClassName('error');
+
+  for (var i = 0; i < users.length; i++) {
+    if (uname === users[i].username) {
+      if (pass === users[i].password) {
+        return true;
+      } else {
+        error[0].innerHTML = '<p>You entered wrong password</p>';
+        return false;
+      }
+    }
+  }
+
+  error[0].innerHTML = '<p>You entered wrong username</p>';
+
+  return false;
 }
