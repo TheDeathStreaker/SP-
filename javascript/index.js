71084dc397959ad3f29224f3ab474e00bbcfd69d
Adrian Jarc <adrianjarc1@gmail.com>
2016-10-21 12:04:41 +0200

    Acnowledge roles on login

--- javascript/index.js
+++ javascript/index.js
@@ -2,12 +2,20 @@ var users = [
   {
     'id': 1,
     'username': 'adrianj',
-    'password': 'adrian1234'
+    'password': 'adrian1234',
+    "role": "student"
   },
   {
     'id': 2,
     'username': 'janezn',
-    'password': 'novak4321'
+    'password': 'novak4321',
+    "role": "professor"
+  },
+  {
+    'id': 3,
+    'username': 'metak',
+    'password': 'metkriz12',
+    "role": "referat"
   }
 ]
 
@@ -22,9 +30,23 @@ var login = function () {
   for (var i = 0; i < users.length; i++) {
     if (uname === users[i].username) {
       if (pass === users[i].password) {
+        switch (users[i].role){
+          case 'professor':
+            document.forms['loginForm'].action = 'professor.html'
+            break;
+          case 'referat':
+            document.forms['loginForm'].action = 'referat.html'
+            break;
+          case 'student':
+            document.forms['loginForm'].action = 'student.html'
+            break;
+          default:
+            document.forms['loginForm'].action = 'student.html'
+        }
         return true;
       } else {
         error[0].innerHTML = '<p>You entered wrong password</p>';
+
         return false;
       }
     }
