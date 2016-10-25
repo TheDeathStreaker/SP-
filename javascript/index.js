d52d80a9ed2a4a7b73477726e4a7be263c1a6119
TheDeathStreaker <adrianjarc1@gmail.com>
2016-10-25 10:51:16 +0200

    Seperate data from main js

--- javascript/index.js
+++ javascript/index.js
@@ -1,26 +1,3 @@
-var users = [
-  {
-    'id': 1,
-    'username': 'adrianj',
-    'password': 'adrian1234',
-    "role": "student"
-  },
-  {
-    'id': 2,
-    'username': 'janezn',
-    'password': 'novak4321',
-    "role": "professor"
-  },
-  {
-    'id': 3,
-    'username': 'metak',
-    'password': 'metkriz12',
-    "role": "referat"
-  }
-]
-
-
-
 var login = function () {
   var uname = document.forms['loginForm']['username'].value;
   var pass = document.forms['loginForm']['password'].value;
---
+++ json/data.js
@@ -0,0 +1,20 @@
+var users = [
+  {
+    'id': 1,
+    'username': 'adrianj',
+    'password': 'adrian1234',
+    "role": "student"
+  },
+  {
+    'id': 2,
+    'username': 'janezn',
+    'password': 'novak4321',
+    "role": "professor"
+  },
+  {
+    'id': 3,
+    'username': 'metak',
+    'password': 'metkriz12',
+    "role": "referat"
+  }
+];
