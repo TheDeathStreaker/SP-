385926b475d4a98cd6b14f4c382284652e204595
TheDeathStreaker <adrianjarc1@gmail.com>
2016-10-29 13:16:00 +0200

    Add classes to data.js

--- json/data.js
+++ json/data.js
@@ -3,18 +3,35 @@ var users = [
     'id': 1,
     'username': 'adrianj',
     'password': 'adrian1234',
-    "role": "student"
+    'role': 'student',
+    'enrolled': [
+      {
+        'id': 1
+      }
+    ]
   },
   {
     'id': 2,
     'username': 'janezn',
     'password': 'novak4321',
-    "role": "professor"
+    'role': 'professor',
+    'holding': [1,2]
   },
   {
     'id': 3,
     'username': 'metak',
     'password': 'metkriz12',
-    "role": "referat"
+    'role': 'referat'
+  }
+];
+
+var classes = [
+  {
+    'id': 1,
+    'name': 'Spletno programiranje'
+  },
+  {
+    'id': 2,
+    'name': 'Something'
   }
 ];
